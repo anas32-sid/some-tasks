ddab5e74938f481091a64e9d2e70afdd704e43d4
mohd. anas <anas9650731343@gmail.com>
2026-04-16 21:40:27 +0530

    Add files via upload

---
+++ classs/class7.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class marks{
+    public:
+char getgrade(int n){
+    if(n>=90){
+    return 'A';
+    }
+    else if(n>=75){
+        return 'B';
+    }
+    else if(n>=50){
+        return 'c';
+    }
+    else{
+        return 'F';
+    }
+}
+};
+
+int main(){
+marks a;
+int n;
+cout<<"enter marks: ";
+cin>>n;
+if(n>0 && n<100){
+    char grade=a.getgrade(n);
+cout<<"Grade: "<<grade;
+}
+else{
+    cout<<"eror 404";
+}
+}
